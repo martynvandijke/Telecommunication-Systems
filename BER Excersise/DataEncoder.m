b5b975f641f7d0b1949353e261c539f09552182d
Martyn van Dijke <m.v.dijke@student.tue.nl>
2016-06-15 10:55:02 +0200

    Switched to binary coding since a lot of errors are caused  by decimal

--- BER Excersise/DataEncoder.m
+++ BER Excersise/DataEncoder.m
@@ -7,11 +7,13 @@ n1 = 15;              % Code length for the 15 bit transmission
 k1 = 11;              % Code length for the 15 bit transmission
 
 if start ==1
-Output=     encode(Data,n,k,'hamming/decimal');
+Var =     encode(Data,n,k,'hamming');
 elseif start == 2
-Output =    encode(Data,n1,k1,'hamming/decimal');
+Var =    encode(Data,n1,k1,'hamming');
 else
     disp('Wrong input');  
 end
+
+Output = Var; %de2bi(Var);
 end
 
--- BER Excersise/main.m
+++ BER Excersise/main.m
@@ -7,6 +7,16 @@
 clear all;
 clc; 
 close all;
+% 
+x = ltePRBS(1,(2^7 -1 ));
+x = single(x);
+s = DataEncoder(1,x);
+s = Noise(s);
+s= Decision(s);
+Out = DataDecoder(1,s' );
+%Out == x;
+
+
 
 %% PRBS generation (using the LTE Systems Toolbox)
 Seq1 = ltePRBS(1,(2^7 -1 ));
@@ -16,9 +26,12 @@ Seq2 = ltePRBS(1,(2^11 -1 ));
 Seq1 = single(Seq1);
 Seq2 = single(Seq2);
 
+%calculate the amount of times Seq1 fits in 10^6 and put Seq1 that many
+%times in a new array who's kength is roughly 10^6
 n1 = round(10^6/length(Seq1));
 Seq33 = repmat(Seq1',1,n1);
 
+%smae but for Seq2
 n2 = round(10^6/length(Seq2));
 Seq22 = repmat(Seq2',1,n2);
 
@@ -31,60 +44,60 @@ step = floor( length(Seq33)/length(snr));
 step2 = floor( length(Seq22)/length(snr));
 
 %% Seq 3
-%add Gaussian noise to the seqence
+%Take the reference Seq and add Gaussian noise to this seqence
 Noisy = Noise(Seq33);
 %make a desicion if the recieved bit is a 0 or a 1
 Decided = Decision(Noisy);
-%calculate the amount of errors for the reference signal
+%calculate the amount of errors the desiced signal has as comapred to 
+%the reference signal
 ErrorRow = biterr(Decided',Seq33,'row-wise');
 %ErrorRow = ((Decided' == Seq33)==0);
 
 %Process the 7 bit transmission system
-EncodedData1 = DataEncoder(1,Seq33);
+EncodedData1 = (DataEncoder(1,Seq33));
+%EncodedDataV2 = EncodedData1(1,:);
 Noisy1 = Noise(EncodedData1);
 Decided1 = Decision(Noisy1);
 %decode the stream of bits
-DecodedData1 = DataDecoder(1,Decided);
-%caclulate the amount of errors & error rate
-ErrorRow1 = biterr(DecodedData1',Seq33,'row-wise');
+DecodedData1 = DataDecoder(1,Decided1');
+%caclulate the amount of errors 
+ErrorRow1 = biterr(DecodedData1,Seq33,'row-wise');
 
+return;
 %Process the 15 bit transmission system
 EncodedData2 = DataEncoder(2,Seq33);
 Noisy2 = Noise(EncodedData2);
 Decided2 = Decision(Noisy2);
-DecodedData2 = DataDecoder(2,Decided2);
-ErrorRow2 = biterr(DecodedData2',Seq33,'row-wise');
+DecodedData2 = DataDecoder(2,Decided2');
+ErrorRow2 = biterr(DecodedData2,Seq33,'row-wise');
 
 %% Seq 2 
-%add Gaussian noise to the seqence
 Noisy = Noise(Seq22);
-%make a desicion if the recieved bit is a 0 or a 1
 Decided01 = Decision(Noisy);
-%calculate teh amount of errors for the reference signal
 ErrorRow01 = biterr(Decided01',Seq22,'row-wise');
+
 %Process the 7 bit transmission system
 EncodedData11 = DataEncoder(1,Seq22);
 Noisy11 = Noise(EncodedData11);
 Decided11 = Decision(Noisy11);
-%decode the stream of bits
-DecodedData11 = DataDecoder(1,Decided11);
-%caclulate the amount of errors & error rate
-ErrorRow11 = biterr(DecodedData11',Seq22,'row-wise');
+DecodedData11 = DataDecoder(1,Decided11');
+ErrorRow11 = biterr(DecodedData11,Seq22,'row-wise');
+
 %Process the 15 bit transmission system
 EncodedData21 = DataEncoder(2,Seq22);
 Noisy21 = Noise(EncodedData21);
 Decided21 = Decision(Noisy21);
-DecodedData21 = DataDecoder(2,Decided21);
-ErrorRow21 = biterr(DecodedData21',Seq22,'row-wise');
+DecodedData21 = DataDecoder(2,Decided21');
+ErrorRow21 = biterr(DecodedData21,Seq22,'row-wise');
 
-for i = 1:1:41
-    ErrorArray_temp(i) = sum( (ErrorRow((step*i - step+1):(step*i)) ));
+parfor i = 1:1:41
+    ErrorArray_temp(i) = sum( (ErrorRow(        (step*i - step+1):(step*i)) ));
     %ErrorArray_tempTest(i) = cumsum( (ErrorRow(p:(step*p)) ))
-    ErrorArray1_temp(i) = sum( ( ErrorRow1((step*i - step +1):(step*i)) ));
-    ErrorArray2_temp(i) = sum(  (ErrorRow2((step*i - step +1):(step*i)) ));
-    ErrorArray11_temp(i) = sum( (ErrorRow11((step2*i - step2 +1):(step2*i)) ));
-    ErrorArray01_temp(i) = sum( (ErrorRow01((step2*i - step2 +1):(step2*i)) ));
-    ErrorArray21_temp(i) = sum( (ErrorRow21((step2*i - step2 +1):(step2*i)) ));
+    ErrorArray1_temp(i) = sum( ( ErrorRow1(     (step*i - step +1):(step*i)) ));
+    ErrorArray2_temp(i) = sum(  (ErrorRow2(     (step*i - step +1):(step*i)) ));
+    ErrorArray11_temp(i) = sum( (ErrorRow11(    (step2*i - step2 +1):(step2*i)) ));
+    ErrorArray01_temp(i) = sum( (ErrorRow01(    (step2*i - step2 +1):(step2*i)) ));
+    ErrorArray21_temp(i) = sum( (ErrorRow21(    (step2*i - step2 +1):(step2*i)) ));
 end
 
 % for p = 1:41
@@ -105,8 +118,10 @@ end
 % ErrorArray21 = (step2 / ErrorArray21_temp);
 
 ErrorArray = (ErrorArray_temp ./ length(Seq33) );
-ErrorArray1 = (ErrorArray1_temp ./ length(Seq33)) ;
-ErrorArray2 = (ErrorArray2_temp ./ length(Seq33));
+ErrorArray1 = (ErrorArray1_temp ./ step) ;
+ErrorArray2 = (ErrorArray2_temp ./ step);
+% ErrorArray1 = (ErrorArray1_temp ./ length(Seq33)) ;
+% ErrorArray2 = (ErrorArray2_temp ./ length(Seq33));
 
 ErrorArray01 = (ErrorArray01_temp ./ length(Seq22));
 ErrorArray11 = (ErrorArray11_temp ./ length(Seq22));
@@ -125,23 +140,23 @@ snr = -20:20;
 
 %% Coding Gain
 %how much can the S/N ratio degrade while keeping the same BER
-parfor p = 1:41
-    for o = 1:41
-        if (ErrorArray1(p) > 0.95*ErrorArray(o) && ErrorArray1(p) < 1.05*ErrorArray(o) )
-        Gain(p) = o-p
-        end
-        if (ErrorArray11(p) > 0.95*ErrorArray01(o) && ErrorArray11(p) < 1.05*ErrorArray01(o) )
-        Gain1(p) = o-p
-        end
-        
-        if (ErrorArray2(p) > 0.95*ErrorArray(o) && ErrorArray2(p) < 1.05*ErrorArray(o) )
-        Gain01(p) = o-p
-        end
-        if (ErrorArray21(p) > 0.95*ErrorArray01(o) && ErrorArray21(p) < 1.05*ErrorArray01(o) )
-        Gain11(p) = o-p
-        end
-    end
-end
+% parfor p = 1:41
+%     for o = 1:41
+%         if (ErrorArray1(p) > 0.95*ErrorArray(o) && ErrorArray1(p) < 1.05*ErrorArray(o) )
+%         Gain(p) = o-p
+%         end
+%         if (ErrorArray11(p) > 0.95*ErrorArray01(o) && ErrorArray11(p) < 1.05*ErrorArray01(o) )
+%         Gain1(p) = o-p
+%         end
+%         
+%         if (ErrorArray2(p) > 0.95*ErrorArray(o) && ErrorArray2(p) < 1.05*ErrorArray(o) )
+%         Gain01(p) = o-p
+%         end
+%         if (ErrorArray21(p) > 0.95*ErrorArray01(o) && ErrorArray21(p) < 1.05*ErrorArray01(o) )
+%         Gain11(p) = o-p
+%         end
+%     end
+% end
 
 
 %% Plot of BER
@@ -165,13 +180,13 @@ h= legend('Reference','7 Bit Hammning coder','15 Bit Hammning coder');
 set(h,'Interpreter','latex');
 xlabel('SNR [dB]','Interpreter','latex')
 ylabel('Bit Error Rate','Interpreter','latex')
-
-%%coding gain
-figure('Name','Gain');
-plot(snr,abs(Gain),snr,abs(Gain01),snr,abs(Gain1),snr,abs(Gain11)         );
-hold on;
-grid on;
-h= legend('Seqence 3 7 bit transmission','Seqence 3 15 bit transmission','Seqence 2 7 bit transmission','Seqence 2 15 bit transmission','Seqence 1 7 bit transmission','Seqence 1 15 bit transmission');
-set(h,'Interpreter','latex');
-xlabel('SNR [dB]','Interpreter','latex')
-ylabel('Coding Gain','Interpreter','latex')
+% 
+% %%coding gain
+% figure('Name','Gain');
+% plot(snr,abs(Gain),snr,abs(Gain01),snr,abs(Gain1),snr,abs(Gain11)         );
+% hold on;
+% grid on;
+% h= legend('Seqence 3 7 bit transmission','Seqence 3 15 bit transmission','Seqence 2 7 bit transmission','Seqence 2 15 bit transmission','Seqence 1 7 bit transmission','Seqence 1 15 bit transmission');
+% set(h,'Interpreter','latex');
+% xlabel('SNR [dB]','Interpreter','latex')
+% ylabel('Coding Gain','Interpreter','latex')
